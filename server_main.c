e972510533330bc36fb088ed90d348cac989eced
ohhyun-kwon <venturekwon@gmail.com>
2018-04-19 17:35:40 +0900

    success thread_pool

--- server_main.c
+++ server_main.c
@@ -10,31 +10,27 @@
 #include <stdio.h>
 #include "apue.h"
 #include <pthread.h>
+#include <unistd.h>
 /*---------------------------------------------------------------- Headers }}}*/
 
 #define MAX_PTHREAD 100
-#define CREATE_PTHREAD 3
+#define CREATE_PTHREAD 5
 
 
+typedef struct thread_info thread_info;
 struct thread_info
 {
     int tid;
+    // 0 idle : 1 working
     int status;
-    int fd[2];
+    int pipes[2];
 };
 
+typedef struct thread_pool thread_pool;
 struct thread_pool
 {
-    struct thread_info[CREATE_PTHREAD];
-};
+    thread_info ti[CREATE_PTHREAD];
 
-// Linked list로 저장하는 것!
-struct thread_pool
-{
-    int tid;
-    int status;
-    // 0 idle / 1 working
-    int tid_a[CREATE_PTHREAD];
 };
 
 /*
@@ -52,44 +48,64 @@ thr_write_fn(void *arg)
 */
 
 void *
-thr_wait(struct thread_pool *tp)
+thr_init_wait(void *tpp)
 {
-    static int i=0;
+    thread_info *ti = (thread_info *)tpp;
+    char buffer[256];
 
-    tp->tid = pthread_self();
-    tp->status = 0;
-    tp->tid_a[i]=tp->tid;
-    printf("%d번째 %d tid %d\n",i,tp->tid,tp->status);
-    ++i;
-    sleep(1);
+    // init
+    ti->tid = pthread_self();
+    ti->status = 0;
+    pipe(ti->pipes);
+
+    printf("tid %d status %d %d pipes\n",
+           ti->tid, ti->status,
+           ti->pipes[0], ti->pipes[1]);
+
+    // wait
+    while(1){
+        printf("11111\n");
+        /*
+         * read는 non-blocking이기에 cpu 걱정 ㄴㄴ + while 쓴것은
+         * 한번만 수행할 것이 아니기에
+         */
+        if(read(ti->pipes[0],buffer,sizeof(buffer)) != -1){
+            // 수행할 것들...
+            printf("22222\n");
+        }
+    }
+
+    // 수행한 결과에 대해서 보내준다.
 }
-
+/*
 void thr_get()
 {
 
 }
+*/
 int main(void)
 {
     printf("Program will start soon!!!\n");
     // Thread pool
     int i;
-    int j;
-    int k;
     int err;
-    struct thread_pool tp;
+    thread_pool tp;
+    thread_info ti;
     pthread_t ntid;
 
-    if(CREATE_PTHREAD<MAX_PTHREAD)
+    if(CREATE_PTHREAD<MAX_PTHREAD){
         for(i=0; i<CREATE_PTHREAD; i++)
         {
-            err = pthread_create(&ntid, NULL, thr_wait, &tp);
-            if (err !=0
+            printf("%d thread\n", i);
+            err = pthread_create(&ntid, NULL, thr_init_wait, &(tp.ti[i]));
+            if (err !=0)
                 printf("Cannot Create thread!!\n");
             sleep(1);
 
         }
+    }
     else
-     printf("Too much for thread pool!!\n");
+        printf("Too much for thread pool!!\n");
 
 // Control Thread는 IO Multiplexing을 하면서 request Message output message를 처리
 
